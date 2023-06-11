9736a9d8e887c4f098489ff8cb074c43d346160f
Matus Leng <m.leng@student.tudelft.nl>
2023-06-12 01:49:02 +0200

    added GPS filtering switch

--- ImageScraper.py
+++ ImageScraper.py
@@ -22,6 +22,7 @@ import re
 # custom patch libraries
 import patch
 
+
 def get_exif_data(img):
     exif_data = img._getexif()
     if exif_data is None:
@@ -45,10 +46,10 @@ def get_exif_data(img):
 
 class ImageScraper:
 
-    def __init__(self, url, webdriver_path, image_path, number_of_images=1, headless=True,
+    def __init__(self, url, webdriver_path, image_path, search_image, filter_by_gps, number_of_images=1, headless=True,
                  min_resolution=(0, 0), max_resolution=(1920, 1080), keep_filenames=False, shift=1):
         # check parameter types
-        image_path = os.path.join(image_path, url[1])
+        image_path = os.path.join(image_path, search_image)
         if not os.path.exists(image_path):
             print("[INFO] Image path not found. Creating a new folder.")
             os.makedirs(image_path)
@@ -60,25 +61,11 @@ class ImageScraper:
                 exit(
                     "[ERR] Please update the chromedriver.exe in the webdriver folder according to your chrome version:https://chromedriver.chromium.org/downloads")
 
-        for i in range(1):
-            try:
-                # try going to url
-                options = Options()
-                if headless:
-                    options.add_argument('--headless')
-                driver = webdriver.Chrome(webdriver_path, chrome_options=options)
-                driver.set_window_size(1400, 1050)
-                driver.get(url[0])
-
-            except Exception as e:
-                # update chromedriver
-                pattern = '(\d+\.\d+\.\d+\.\d+)'
-                version = list(set(re.findall(pattern, str(e))))[0]
-                is_patched = patch.download_lastest_chromedriver(version)
-                if (not is_patched):
-                    exit(
-                        "[ERR] Please update the chromedriver.exe in the webdriver folder according to your chrome "
-                        "version:https://chromedriver.chromium.org/downloads")
+        options = Options()
+        if headless:
+            options.add_argument('--headless')
+        driver = webdriver.Chrome(webdriver_path, chrome_options=options)
+        driver.set_window_size(1400, 1050)
 
         self.driver = driver
         self.number_of_images = number_of_images
@@ -90,62 +77,9 @@ class ImageScraper:
         self.max_resolution = max_resolution
         self.search_key = "image"
         self.keep_filenames = keep_filenames
-        self.shift = shift
-        self.total_images = int(driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/span/i').text)
-        self.last_url = self.url
         self.index = 0
-
-    def find_image_urls(self):
-        """
-            This function search and return a list of image urls based on the search key.
-        """
-        wait = WebDriverWait(self.driver, 10)
-        image_urls = []
-        print("[INFO] Gathering image links")
-
-        try:
-            self.driver.get(self.url)
-            image_url = self.driver.find_element_by_xpath('/html/body/div[3]/div/ul/li[1]/a[1]').get_attribute("href")
-            self.driver.get(image_url)
-        except Exception as e:
-            print(e)
-            self.driver.get(self.url)
-        time.sleep(3)
-        search_string = '/html/body/div[3]/div/div[1]/div[1]/ul/li/img'
-        running = True
-        self.index = 0 + self.shift
-        # for i in range(500):
-        while running:
-            self.last_url = self.driver.current_url
-            self.index += 1
-            progress_pct = self.index / self.total_images * 100
-            if self.index % 100 == 0:
-                print(f"Progress: {self.index}/{self.total_images} ({progress_pct:.1f}%)")
-            # if len(image_urls) > 10:
-            #     new_list = image_urls[:]
-            #     asyncio.run(self.save_images(new_list, self.keep_filenames))
-            # for _ in range(100):
-            time.sleep(0.3)
-            wait.until_not(EC.visibility_of_element_located((By.CSS_SELECTOR, "block-loading _j_stageloading")))
-            for i in range(3):
-                image = self.driver.find_element_by_xpath(search_string)
-                src_link = image.get_attribute("src")
-                if src_link is not None and src_link != "None":
-                    image_urls.append(src_link)
-                    break
-                time.sleep(0.2)
-            for i in range(10):
-                try:
-                    button = self.driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/div[1]/a[2]")
-                    button.click()
-                    break
-                except Exception as e:
-                    time.sleep(1)
-                    if i == 9:
-                        running = False
-        self.driver.quit()
-        print("[INFO] URL gathering ended")
-        return image_urls
+        self.search_image = search_image
+        self.filter_by_gps = filter_by_gps
 
     def save_images(self, image_urls_og, keep_filenames):
         image_urls = image_urls_og[:]
@@ -162,6 +96,7 @@ class ImageScraper:
         """
         print("[INFO] Saving image, please wait...")
         failed_downloads = []
+        breakpoint()
         for indx, image_url in enumerate(image_urls):
             try:
                 print("[INFO] Image url:%s" % (image_url))
@@ -169,7 +104,7 @@ class ImageScraper:
                 image = requests.get(image_url, timeout=5)
                 if image.status_code == 200:
                     with Image.open(io.BytesIO(image.content)) as image_from_web:
-                        if get_exif_data(image_from_web) is not None:
+                        if get_exif_data(image_from_web) is not None or self.filter_by_gps is False:
                             # if exif_data.items().__contains__()
                             try:
                                 if keep_filenames:
@@ -181,24 +116,40 @@ class ImageScraper:
                                     filename = "%s.%s" % (name, image_from_web.format.lower())
                                 else:
                                     filename = "%s%s.%s" % (
-                                        search_string, str(indx + self.shift), image_from_web.format.lower())
+                                        search_string, str(indx), image_from_web.format.lower())
 
                                 image_path = os.path.join(self.image_path, filename)
                                 print(
-                                    f"[INFO] {self.search_key} \t {indx + self.shift} \t Image saved at: {image_path}")
-                                if image_path.endswith(".gif"):
+                                    f"[INFO] {self.search_key} \t {indx} \t Image saved at: {image_path}")
+                                exif = image_from_web.info.get("exif")
+                                if self.filter_by_gps is True:
 
-                                    image_from_web.save(image_path, save_all=True, exif=image_from_web.info.get("exif"))
+                                    if image_path.endswith(".gif"):
 
+                                        image_from_web.save(image_path, save_all=True, exif=exif)
+
+                                    else:
+                                        image_from_web.save(image_path, exif=exif)
                                 else:
-                                    image_from_web.save(image_path, exif=image_from_web.info.get("exif"))
+                                    if image_path.endswith(".gif"):
+
+                                        image_from_web.save(image_path, save_all=True)
+
+                                    else:
+                                        image_from_web.save(image_path)
 
                             except OSError:
                                 rgb_im = image_from_web.convert('RGB')
-                                if image_path.endswith(".gif"):
-                                    rgb_im.save(image_path, save_all=True, exif=rgb_im.info.get("exif"))
+                                if self.filter_by_gps is True:
+                                    if image_path.endswith(".gif"):
+                                        rgb_im.save(image_path, save_all=True, exif=exif)
+                                    else:
+                                        rgb_im.save(image_path, exif=exif)
                                 else:
-                                    rgb_im.save(image_path, exif=rgb_im.info.get("exif"))
+                                    if image_path.endswith(".gif"):
+                                        rgb_im.save(image_path, save_all=True)
+                                    else:
+                                        rgb_im.save(image_path)
                             image_resolution = image_from_web.size
                             if image_resolution is not None:
                                 if image_resolution[0] < self.min_resolution[0] or image_resolution[1] < \
@@ -226,8 +177,6 @@ class ImageScraper:
         print(
             f'[INFO] Downloads completed.')
 
-        self.shift += len(image_urls)
-
     def check_failed_downloads(self):
         if os.path.exists('failed_downloads.txt'):
             with open('failed_downloads.txt', 'r') as file:
@@ -248,22 +197,49 @@ class ImageScraper:
                     break
 
     def scroll_page_for_links(self):
+        # breakpoint()
         elements = []
         while len(elements) < 1000:
             # Scroll down to the bottom of the webpage
-            actions = ActionChains(self.driver)
-            actions.send_keys(Keys.END).perform()
-            time.sleep(1)
+            self.driver.execute_script("window.scrollBy(0, window.innerHeight)")
 
             # Find all the elements matching the xpath
-            elements = self.driver.find_elements(By.CLASS_NAME, "general-imgcol-item")
+
+            elements = self.driver.find_elements(By.XPATH, "//a[@class='general-imgcol-item']/img")
             print(len(elements))
-        pass
+            time.sleep(1)
+
+            # text_content = "没有更多"
+            # tag_name = "div"
+            xpath_expression = "//*[contains(text(), '没有更多') and contains(@class, 'graph-similar-list-bottom-status')]"
+            try:
+                element = self.driver.find_element_by_xpath(xpath_expression)
+                if element is not None:
+                    style = element.get_attribute("style")
+                    if style == "":
+                        break
+            except Exception as e:
+                pass
+        img_links = list(map(lambda x: x.get_attribute("src"), elements))
+        return img_links
+
+    def search_for_picture(self):
+        self.driver.get(self.url)
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(
+            EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div[1]/div[7]/div/span[1]/span[1]')))
+        element.click()
+        time.sleep(1)
+
+        image_path = os.getcwd() + "/images/" + self.search_image
+        file_input_element = self.driver.find_element_by_class_name("general-upload-file")
+        file_input_element.send_keys(image_path)
 
     def run_scraper(self):
-        self.driver.get("https://graph.baidu.com/s?sign=1267819133b6badfea32501686460968&f=all&tn=pc&tn=pc&idctag=gz&idctag=gz&sids=&sids=&logid=2823725675&logid=2823725675&pageFrom=graph_upload_bdbox&pageFrom=graph_upload_pcshitu&srcp=&gsid=&session_id=3291421184242315508&extUiData%5BisLogoShow%5D=1&tpl_from=pc&entrance=general")
+        self.search_for_picture()
         wait = WebDriverWait(self.driver, 10)
-        element = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div[3]/div/div[2]/div/div[1]/a[1]')))
+        element = wait.until(
+            EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div[3]/div/div[2]/div/div[1]/a[1]')))
         image_urls = self.scroll_page_for_links()
-
-
+        self.driver.quit()
+        self.save_images(image_urls,keep_filenames=self.keep_filenames)
--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from ImageScraper import ImageScraper
 from patch import webdriver_executable
 
 
-def worker_thread(url):
+def worker_thread(search_image):
     image_scraper = ImageScraper(
+        search_image=search_image,
         url=url,
         webdriver_path=webdriver_path,
         image_path=image_path,
         headless=headless,
         min_resolution=min_resolution,
         max_resolution=max_resolution,
-        keep_filenames=keep_filenames)
+        keep_filenames=keep_filenames,
+        filter_by_gps=filter_by_gps)
 
     image_scraper.run_scraper()
     # Release resources
@@ -22,12 +24,7 @@ def worker_thread(url):
 # Define file path
 image_path = os.path.normpath(os.path.join(os.getcwd(), 'photos'))
 webdriver_path = os.path.normpath(os.path.join(os.getcwd(), 'webdriver', webdriver_executable()))
-
-"""
-put your urls and folder names below
-Example: urls = [("web1.com","folder_name1"),("url2.com","folder_name2")]
-"""
-urls = [("https://www.mafengwo.cn/photo/poi/23372.html", "mywebsite")]
+url = ("https://graph.baidu.com/pcpage/index?tpl_from=pc", "baidu")
 
 # Parameters
 headless = False  # True = No Chrome GUI
@@ -35,13 +32,14 @@ min_resolution = (0, 0)  # Minimum desired image resolution
 max_resolution = (9999, 9999)  # Maximum desired image resolution
 keep_filenames = False  # Keep original URL image filenames
 
+# PUT IMAGES TO SEARCH IN "images" folder and write their names here
+images = {"DSCN0010.jpg"}
+filter_by_gps = True   # True = only images with exif GPS data will be downloaded
 
-# Run each url in a separate thread
-# Automatically waits for all threads to finish
 
 def main():
-    with concurrent.futures.ThreadPoolExecutor(max_workers=len(urls)) as executor:
-        executor.map(worker_thread, urls)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(images)) as executor:
+        executor.map(worker_thread, images)
 
 
 if __name__ == '__main__':
